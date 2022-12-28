74f78314e6ff12bbe2402d264d97b209383c79d8
Vitor Veloso <vitor.asv@gmail.com>
2022-12-28 18:29:39 -0300

    Trying to fix images

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ export default function Card(props) {
     return (
         <div className='card'>
             <div className='card-image-container'>
-                <img src={`./../../../public/public-assets/${props.imageUrl}`} alt={props.title} />
+                <img src={`%PUBLIC_URL%/${props.imageUrl}`} alt={props.title} />
             </div>
             <div className='card-info'>
                 <img className='map-pin' src={pin} alt="map pin" />
