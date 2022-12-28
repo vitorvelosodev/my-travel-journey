2e36e453239f6ed240b6496927dcf79a46e78845
Vitor Veloso <vitor.asv@gmail.com>
2022-12-27 23:25:40 -0300

    data_vitor.js updated

--- src/assets/data_vitor.js
+++ src/assets/data_vitor.js
@@ -1,11 +1,47 @@
 export default [
     {
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        title: "Vila Velha",
+        location: "Espírito Santo",
+        googleMapsUrl: "https://goo.gl/maps/xSifD9voD2Trz8Ki8",
+        startDate: "11 Jun, 2022",
+        endDate: "12 Jul, 2022",
+        description: "Vila Velha é um município brasileiro localizado no litoral do estado do Espírito Santo, na Região Sudeste do país. Pertence à Região Metropolitana de Vitória e ocupa uma área de 209,965 km², sendo que 54,57 km² estão em perímetro urbano, e a população em 2021 foi estimada pelo IBGE em 508 655 habitantes, o que faz do município o segundo mais populoso do Espírito Santo, atrás apenas da Serra.",
+        imageUrl: "../../public/public-assets/vila-velha.jpeg"
+    },
+    {
+        title: "Itacaré",
+        location: "Bahia",
+        googleMapsUrl: "https://goo.gl/maps/wUfRTm1VXiQdQwAf7",
+        startDate: "23 Mai, 2022",
+        endDate: "28 Mai, 2022",
+        description: "Itacaré é uma pequena cidade localizada no litoral do estado da Bahia, na Costa do Cacau, ao sul de Salvador.",
+        imageUrl: "../../public/public-assets/itacare.jpeg"
+    },
+    {
+        title: "Morro de São Paulo",
+        location: "Bahia",
+        googleMapsUrl: "https://goo.gl/maps/e8p5ZCDC5pFPw6tY8",
+        startDate: "07 Mai, 2022",
+        endDate: "13 Mai, 2022",
+        description: "Morro de São Paulo é um destino com praias incríveis, localizadas a apenas 60 km de Salvador, na Bahia. O lugar é perfeito para quem ama relaxar em faixas de areia paradisíacas, mergulhar em um mar transparente, comer boas moquecas, praticar snorkeling ou, melhor ainda, não fazer nada e descansar!",
+        imageUrl: "../../public/public-assets/morro-de-sp.heic"
+    },
+    {
+        title: "Tamandaré",
+        location: "Pernambuco",
+        googleMapsUrl: "https://goo.gl/maps/Ts9KrCjFxBf4ZXEh6",
+        startDate: "23 Out, 2021",
+        endDate: "27 Out, 2021",
+        description: "Tamandaré é um município do litoral sul do estado de Pernambuco, na Região Nordeste do Brasil. Pertence à Região Geográfica Intermediária do Recife e à Região Geográfica Imediata de Barreiros-Sirinhaém, localizando-se 109 quilômetros a sul da capital pernambucana.",
+        imageUrl: "../../public/public-assets/tamandare.jpeg"
+    },
+    {
+        title: "João Pessoa",
+        location: "Paraíba",
+        googleMapsUrl: "https://goo.gl/maps/djv63TW6M4riP4Ef7",
+        startDate: "19 Set, 2021",
+        endDate: "04 Out, 2021",
+        description: "João Pessoa é uma cidade costeira próxima da foz do rio Paraíba, no leste do Brasil. A sua cidade velha é conhecida pela arquitetura barroca e art nouveau.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     }
 ]
